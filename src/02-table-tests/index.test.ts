4e1931352fcfb26cc0cb6dba7ec26f5c1314ddfd
Meruert <mika.amantaikuzu@gmail.com>
2024-10-18 00:08:15 +0500

    feat: complete tests 02-table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,44 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  // add two numbers
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: -2, b: 8, action: Action.Add, expected: 6 },
+  // substrack two numbers
+  { a: 5, b: 2, action: Action.Subtract, expected: 3 },
+  { a: -2, b: 8, action: Action.Subtract, expected: -10 },
+  { a: -8, b: -2, action: Action.Subtract, expected: -6 },
+  // multiply two numbers
+  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
+  { a: -2, b: 3, action: Action.Multiply, expected: -6 },
+  { a: 1, b: 0, action: Action.Multiply, expected: 0 },
+  // divide two numbers
+  { a: 8, b: 2, action: Action.Divide, expected: 4 },
+  { a: 3, b: 6, action: Action.Divide, expected: 0.5 },
+  { a: 0, b: 3, action: Action.Divide, expected: 0 },
+  // exponentiate two numbers
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  { a: -2, b: 3, action: Action.Exponentiate, expected: -8 },
+  // return null for invalid action
+  { a: 9, b: 3, action: '%', expected: null },
+  { a: 9, b: 3, action: null, expected: null },
+  { a: 9, b: 3, action: undefined, expected: null },
+  { a: 9, b: 3, action: '', expected: null },
+  { a: 9, b: 3, action: 'remainder', expected: null },
+  { a: 9, b: 3, action: true, expected: null },
+  { a: 9, b: 3, action: false, expected: null },
+  // return null for invalid arguments
+  { a: '', b: 3, action: Action.Add, expected: null },
+  { a: true, b: 3, action: Action.Subtract, expected: null },
+  { a: 'false', b: 3, action: Action.Multiply, expected: null },
+  { a: null, b: 3, action: Action.Divide, expected: null },
+  { a: undefined, b: 3, action: Action.Divide, expected: null },
+  { a: '9', b: 3, action: Action.Divide, expected: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('Numbers $a and $b in the action $action should return $expected', ({a, b, action, expected}) => {
+    expect(simpleCalculator({a, b, action})).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
